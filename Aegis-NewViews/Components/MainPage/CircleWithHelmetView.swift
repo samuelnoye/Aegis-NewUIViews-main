24d2d462e9940a0cffb7c97ad24798bce9c5fdc3
Samuel Noye <samuel.noye@amalitech.org>
2022-01-16 18:45:57 +0000

    update

--- Aegis-NewViews/Components/MainPage/CircleWithHelmetView.swift
+++ Aegis-NewViews/Components/MainPage/CircleWithHelmetView.swift
@@ -22,15 +22,20 @@ struct CircleWithHelmetView: View {
                 ZStack(){
                     Circle() //width: 150, height:75 //width: 130, height: 65
                         .fill(Color("CircleBorderColor-1"))
-                        .frame(width: Screen.screenSize.width < 375 ? 130 : 150, height: Screen.screenSize.height < 700 ? 65 : 75)
+                        .frame(width: Screen.screenSize.width < 375 ? 140 : 150, height: Screen.screenSize.height < 700 ? 70 : 75)
                     Circle()
                         .fill(Color("MainColor"))
                         .frame(width: Screen.screenSize.width < 375 ? 110 : 130, height: Screen.screenSize.height < 700 ? 55 : 65)
                         .overlay(Circle().stroke(Color("CircleBorderColor"),lineWidth: 4))
                     Image(colorScheme == .light ? "helmet-white" : "helmet-dark")
                 }
-            })
+            }).buttonStyle(FlatLinkStyle())
         }
     }
 }
 
+struct FlatLinkStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+    }
+}
