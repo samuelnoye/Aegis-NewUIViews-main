fb5bdec1d0758da6f5f97ebab2b7f49bc26b9bfd
Samuel Noye <samuel.noye@amalitech.org>
2022-01-18 08:43:15 +0000

    update

---
+++ Aegis-NewViews/Components/MainPage/ProfileIconView.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileIconView.swift
+//  Aegis-NewViews
+//
+//  Created by Samuel Noye on 18/01/2022.
+//
+
+import SwiftUI
+
+struct ProfileIconView: View {
+    var body: some View {
+        ZStack{
+            Circle()
+                .fill(Color("MainColor"))
+                .frame(width: Screen.screenSize.width < 375 ? 50 : 80, height: Screen.screenSize.height < 700 ? 25 : 40)
+            Image(systemName: "person.crop.circle.fill")
+                .frame(width: Screen.screenSize.width < 375 ? 45 : 80, height: Screen.screenSize.height < 700 ? 22.5 : 40)
+            
+        }
+    }
+}
+
+struct ProfileIconView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileIconView().previewDevice("iPhone 8")
+        ProfileIconView().previewDevice("iPhone 11")
+    }
+}
