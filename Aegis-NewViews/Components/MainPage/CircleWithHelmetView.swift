90e51abc3b2484f08e2ae16d1283d4854f385205
Samuel Noye <samuel.noye@amalitech.org>
2022-01-21 17:56:46 +0000

    update

--- Aegis-NewViews/Components/MainPage/CircleWithHelmetView.swift
+++ Aegis-NewViews/Components/MainPage/CircleWithHelmetView.swift
@@ -16,12 +16,13 @@ struct CircleWithHelmetView: View {
             
                 ZStack(){
                     Circle()
-                        .fill(Color("CircleBorderColor-1"))
+                        .fill(Color("CircleBorderColor"))
                         .frame(width: Screen.screenSize.width < 375 ? 136 : 150, height: Screen.screenSize.height < 700 ? 68 : 75)
+                        .overlay(Circle().stroke(Color("CircleBorderColor-1"),lineWidth: 2))
                     Circle()
                         .fill(Color("MainColor"))
                         .frame(width: Screen.screenSize.width < 375 ? 110 : 130, height: Screen.screenSize.height < 700 ? 55 : 65)
-                        .overlay(Circle().stroke(Color("CircleBorderColor"),lineWidth: 4))
+                        
                     Image(colorScheme == .light ? "helmet-white" : "helmet-dark")
                 }
            
