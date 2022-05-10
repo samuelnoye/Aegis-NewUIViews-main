cda2cdd9a852498b046cda1296b112ab331463be
Samuel Noye <samuel.noye@amalitech.org>
2022-05-10 17:59:45 +0000

    Update RidesModel.swift

--- Aegis-NewViews/Model/RidesModel.swift
+++ Aegis-NewViews/Model/RidesModel.swift
@@ -15,13 +15,13 @@ import SwiftUI
 //MARK: - FRUITS DATA MODEL
 
 struct Rides: Identifiable{
-    // var id = UUID()
-    // var username: String
-    // var date: String
-    // var day: String
-    // var distance: String
-    // var duration: String
-    // var speed: String
-    // var video: String
+    var id = UUID()
+    var username: String
+    var date: String
+    var day: String
+    var distance: String
+    var duration: String
+    var speed: String
+    var video: String
     
 }
